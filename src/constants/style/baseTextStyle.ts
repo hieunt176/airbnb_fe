c071a82c26da624d48d4393866559cc1d20ae05d
thaiminhhuy <18520854@gm.uit.edu.vn>
2022-03-24 23:20:53 +0700

    refactor code

---
+++ src/constants/style/baseTextStyle.ts
@@ -0,0 +1,10 @@
+import { TextStyle } from "../../@types/styles/TextStyle";
+
+
+export const baseText: TextStyle = {
+  color: "#fff",
+  fontWeight: "800",
+  textAlign: "start",
+  fontSize: "33px",
+  padding: "0",
+};
