4d49cdd3b7c4d7a32cc2436e854e6bb1a1092e0d
thaiminhhuy <18520854@gm.uit.edu.vn>
2022-04-06 21:14:34 +0700

    fix app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css';
-import AskSuperHost from './pages/ask_super_host';
 import Home from "./pages/home"
 
 
 function App() {
   return (
     <div className="airbnb">
-      <AskSuperHost />
+      <Home />
     </div>
   );
 }
